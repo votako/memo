726b275bfc49d9e7afdc8d8eb8b19c4dffd3d2df
votako <votako@gmail.com>
2021-09-30 19:55:47 +0300

    test

---
+++ src/containers/main/test.tsx
@@ -0,0 +1,13 @@
+import Cart from "../../components/cart";
+import { GetField } from "./getField";
+import { Game, HorizRow, Main as MainDiv } from "./styled";
+
+export const Test = () => {
+  return (
+    <MainDiv>
+      <Game>
+        asd
+      </Game>
+    </MainDiv>
+  );
+};
