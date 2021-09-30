8ee2fe2bc7a8efdff11f1c14b8935f01c53e98c3
votako <votako@gmail.com>
2021-09-30 19:59:01 +0300

    Revert "test"
    This reverts commit 726b275bfc49d9e7afdc8d8eb8b19c4dffd3d2df.

--- src/containers/main/main.tsx
+++ src/containers/main/main.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Cart from "../../components/cart";
 import { GetField } from "./getField";
 import { Game, HorizRow, Main as MainDiv } from "./styled";
@@ -8,9 +9,22 @@ interface fieldInter {
     val: number,
 }
 
+const initalState = {
+    rowcol: false,
+    name: "",
+    row1: [],
+    col: [],
+    val: 0,
+}
+
 export const Main = () => {
   let field = GetField();
+  let [colorState, setColor] = useState("[]");
     
+    // setColor( prevState => ({
+    //     ...prevState,
+    //     row1: colorState.row1.push([1,2,3])
+    // }))
   const onClick = (el: fieldInter) => {
     console.log(el);
     console.log(field[el.row][el.col]);
@@ -23,11 +37,16 @@ export const Main = () => {
           return (
             <HorizRow key={indexRow}>
               { arrRow.map((col, indexCol) => {
+                //   setColor(colorState="asd")
                 return (
                   <Cart
+                    // click={() => onClick("row: " + indexRow + " col: " + indexCol + " val: " + col)}
                     click={() => onClick({row: indexRow, col: indexCol, val: col})}
+                    // click={() => setColor(!colorState)}
                     key={indexRow + " " + indexCol}
                     id={ {id: indexRow + " " + indexCol, isClicked: false} }
+                    // isClicked={colorState ? true : false}
+                    // color={false}
                     value={col}
                   />
                 );
